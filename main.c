e0b43e1abb41ef28b8168365ac784c3befef1fc6
switchcodeur <switchcodeur@switchcodeur.com>
2026-01-09 10:20:41 +0100

    Added server socket creation

--- main.c
+++ main.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 #include <winsock2.h>
+#include <ws2tcpip.h>
 
 static const WORD version = MAKEWORD(2, 2);
+static const char host[] = "localhost";
+static const char port[] = "50000";
 
 int
 main(void)
 {
 	WSADATA wsadata;
+	struct addrinfo *result = NULL, hints;
+	SOCKET server_socket = INVALID_SOCKET;
+	int code;
 	if (WSAStartup(version, &wsadata)) {
 		fprintf(stderr, "Missing the winsock dll");
 		return 1;
 	}
+	ZeroMemory(&hints, sizeof (hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+	code = getaddrinfo(host, port, &hints, &result);
+	if (code != 0) {
+		fprintf(stderr, "getaddrinfo() error: %d\n", code);
+		WSACleanup();
+		return 1;
+	}
+	server_socket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (server_socket == INVALID_SOCKET) {
+		code = WSAGetLastError();
+		fprintf(stderr, "socket() error: %d\n", code);
+		WSACleanup();
+		freeaddrinfo(result);
+		return 1;
+	}
 	puts("Hello world!");
 	WSACleanup();
+	freeaddrinfo(result);
+	closesocket(server_socket);
 	return 0;
 }
